3ee251a0dfa41ec9fad81606be9a67957548c53e
phelipegomes <phelipecedaspy@gmail.com>
2022-04-26 20:25:43 -0300

    including components and modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
+import React from 'react';
+
+import SearchInput from './components/SearchInput/index.js';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <SearchInput />
     </div>
   );
 }
 
-export default App;
---
+++ src/components/SearchInput/index.js
@@ -0,0 +1,8 @@
+export default function SearchInput() {
+    return (
+        <div className="container">
+            <h1>Insert text here</h1>
+            <input type="text"></input>
+        </div>
+    );
+}
