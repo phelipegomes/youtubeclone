901114231616f3ee55924eb92cf9305f1defb981
phelipegomes <phelipecedaspy@gmail.com>
2022-04-26 20:31:46 -0300

    adding new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import SearchInput from './components/SearchInput/index.js';
+import ListVideos from './components/ListVideos/index.js';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
         <SearchInput />
+        <ListVideos />
+        
     </div>
   );
 }
---
+++ src/components/ListVideos/index.js
@@ -0,0 +1,20 @@
+export default function ListVideos() {
+    return (
+        <div className="container list-videos">
+            <ul>
+                <li>
+                    <p>VIDEO 1</p>
+                </li>
+                <li>
+                    <p>VIDEO 2</p>
+                </li>
+                <li>
+                    <p>VIDEO 3</p>
+                </li>
+                <li>
+                    <p>VIDEO 4</p>
+                </li>
+            </ul>
+        </div>
+    );
+}
