27c66ddda3724e1f4535ff91ef0f4d4366fcefde
phelipegomes <phelipecedaspy@gmail.com>
2022-04-26 21:38:20 -0300

    resolve bug in API connection

--- src/components/Api/index.js
+++ src/components/Api/index.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-export default function Api() {
+const Api = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyCHFVBlhOdyhuN-gioJh8oMOJcHnmhGGTo&part=snippet,contentDetails,statistics,status',
+});
 
-    const Api = axios.create({
-        baseURL: 'https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyCHFVBlhOdyhuN-gioJh8oMOJcHnmhGGTo&part=snippet,contentDetails,statistics,status',
-        }
-    );
-}
+export default Api;
--- src/components/ListVideos/index.js
+++ src/components/ListVideos/index.js
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react';
+
+import Api from '../Api/index.js';
+
 export default function ListVideos() {
+
+    const [videos, setVideos ] = useState([123,"oi",11]);
+
+    useEffect(() => {
+        Api
+        .get("")
+        .then((response) => setVideos(response.data))
+        .catch((err) => {
+            console.error("Failed to get API response" + err);
+        });
+
+    }, []);
+
+    console.log(videos);
+
     return (
         <div className="container list-videos">
+           
             <ul>
                 <li>
                     <p>VIDEO 1</p>
